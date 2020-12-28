268d4f725b7bcea33eca1ea613b61767194edaa2
Yashua <yeshuaangelito@gmail.com>
2020-12-28 16:49:58 -0500

    Adding dependencies for mapstruct and plugins

--- brewery/src/main/java/spring/brewery/web/controller/v2/BeerControllerV2.java
+++ brewery/src/main/java/spring/brewery/web/controller/v2/BeerControllerV2.java
@@ -1,5 +1,8 @@
 package spring.brewery.web.controller.v2;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +18,21 @@ import java.util.UUID;
 /**
  *This class handle a get mapping to return back a beer id
  */
+@Slf4j
 @Validated
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
 public class BeerControllerV2 {
 
     private final BeerServiceV2 beerServiceV2;
 
+    /**
+     * commented cause we are using lombock required args constructor
     public BeerControllerV2(BeerServiceV2 beerServiceV2) {
         this.beerServiceV2 = beerServiceV2;
     }
-
+*/
     @GetMapping({"/{beerId}"}) //Path parameter || Path variable
     //I'm being specific in @pathvariable, but it's not necessary 'cause mapping & variable has the same name
     public ResponseEntity<BeerDTOV2> getBeer(@NotNull @PathVariable("beerId") UUID beerId){
@@ -36,9 +43,11 @@ public class BeerControllerV2 {
     @PostMapping //This create a new beer
     public ResponseEntity handlePost (@Valid @RequestBody BeerDTOV2 beerDTO){ //RequestBody is used to get the object, otherwise I get an empty object
 
-        BeerDTOV2 savedDto = beerServiceV2.saveNewBeer(beerDTO);
+        log.debug("LOg test in post mapping..."); //Slf4j
 
-        HttpHeaders headers = new HttpHeaders();
+        val savedDto = beerServiceV2.saveNewBeer(beerDTO);
+
+        val headers = new HttpHeaders();
         //todo add host to utl on "headerValue" actually is the same as http://localhost:8080/api/v1/beer
         headers.add("Location","/api/v2/beer/"+savedDto.getBeerId().toString());
 
